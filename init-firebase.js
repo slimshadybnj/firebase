577717b16c11dc4bf670d796ee7d94025db50e80
Alexander Lee Smith <63920477+lapsangsouchy@users.noreply.github.com>
2023-08-08 22:36:21 -0400

    Refresh to Starter

--- init-firebase.js
+++ init-firebase.js
@@ -2,14 +2,7 @@
  * @TODO replace the firebaseConfig object below with the one
  * provided for you by firebase after creating your project!
  */
-const firebaseConfig = {
-  apiKey: 'AIzaSyCM7RlQ7MVV5ZeXgLUi6EBF-nWOLwLnwsE',
-  authDomain: 'firemessages-a43f5.firebaseapp.com',
-  projectId: 'firemessages-a43f5',
-  storageBucket: 'firemessages-a43f5.appspot.com',
-  messagingSenderId: '1033712355419',
-  appId: '1:1033712355419:web:f7e4dbcea82617b899d77e',
-};
+const firebaseConfig = {};
 
 // initialize the firebase app with this built-in function
 firebase.initializeApp(firebaseConfig);
